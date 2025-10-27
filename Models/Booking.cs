27977cd0be4d3ab611a8e5bf14e904444006f9f2
Sekvoja <41228526+Sekvoja@users.noreply.github.com>
2025-10-27 11:18:36 +0100

    add model bookings

---
+++ Models/Booking.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace BookRoom.Models
+{
+    public class Booking
+    {
+        [JsonPropertyName("hotelId")]
+        public string HotelId { get; set; }
+
+        [JsonPropertyName("arrival")]
+        public string Arrival { get; set; }
+
+        [JsonPropertyName("departure")]
+        public string Departure { get; set; }
+
+        [JsonPropertyName("roomType")]
+        public string RoomType { get; set; }
+
+        [JsonPropertyName("roomRate")]
+        public string RoomRate { get; set; }
+    }
+}
