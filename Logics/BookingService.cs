c10337c8d58112369218a5f128510ce69bff8b3a
Sekvoja <41228526+Sekvoja@users.noreply.github.com>
2025-10-27 11:30:02 +0100

    sketch of the logics

---
+++ Logics/BookingService.cs
@@ -0,0 +1,26 @@
+﻿using BookRoom.Models;
+
+namespace BookRoom.Logics
+{
+    public class BookingService
+    {
+        private List<Hotel> _hotels;
+        private List<Booking> _bookings;
+        public BookingService(List<Hotel> hotels, List<Booking> bookings) 
+        {
+            _hotels = hotels;
+            _bookings = bookings;
+        }
+
+        public string Availability()
+        {
+            return null;
+        }
+
+        public string Search()
+        {
+            return null;
+        }
+
+    }
+}
